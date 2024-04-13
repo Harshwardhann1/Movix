334a9c39535d21d63e5f070b47a2cd058ce69a5d
Harsh Wardhan <wardhan071@gmail.com>
2024-04-13 13:00:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
   const dispatch = useDispatch();
-  const url = useSelector((state) => state.url);
+  const url = useSelector((state) => state.home);
+  console.log(url);
   useEffect(() => {
     apiTesting();
   }, []);
